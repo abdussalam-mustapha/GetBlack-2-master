6cfe08c6b59e3285fdcb2c812eeeaf64b8aab9f3
abdussalam-mustapha <abdussalammustapha07@gmail.com>
2023-02-11 13:30:47 -0800

    new update

--- src/common/signup/Login.jsx
+++ src/common/signup/Login.jsx
@@ -29,54 +29,61 @@ const Login = () => {
 
     return (
         <>
-            {
-                success ? 
-                (
-                    <section>
-                        <p ref={errRef} className={errMsg ? "errmsg" : "offscreen"} aria-live="assertive">{errMsg}</p>
-                        <h1>Sign n</h1>
-                        <form onSubmit={handleSubmit}>
-                            <label htmlFor="username">Username:</label>
-                            <input
-                                type="text"
-                                id="username"
-                                ref={userRef}
-                                autoComplete="off"
-                                onChange={(e) => setUser(e.target.value)}
-                                value={user}
-                                required
-                            />
-                            <label htmlFor="password">Password:</label>
-                            <input
-                                type="password"
-                                id="password"
-                                ref={userRef}
-                                onChange={(e) => setPwd(e.target.value)}
-                                value={pwd}
-                                required
-                            />
-                            <button>Sign In</button>
-                        </form>
-                        <p>
-                            Need an account?
-                            <br />
-                            <span className="line">
-                                {/* put router link */}
-                                <a href="#">Sign Up</a>
-                            </span>
-                        </p>
-                    </section>
-                    ) : 
-                    (
-                    <section>
-                        <h1>you're logged in</h1>
-                        <br />
-                        <p>
-                            <a href="#">Go to home</a>
-                        </p>
-                    </section>
-                )
-            }
+            <section className='login-section'>
+                {
+                    success ?
+                        (
+                            <section className='login'>
+                                <p ref={errRef} className={errMsg ? "errmsg" : "offscreen"} aria-live="assertive">{errMsg}</p>
+                                <h1 className="login_header">Sign in</h1>
+                                <form onSubmit={handleSubmit}>
+                                    {/* <label htmlFor="username">Username:</label> */}
+                                    <input
+                                        type="text"
+                                        id="username"
+                                        placeholder='Username'
+                                        ref={userRef}
+                                        autoComplete="off"
+                                        onChange={(e) => setUser(e.target.value)}
+                                        value={user}
+                                        required
+                                    />
+                                    {/* <label htmlFor="password">Password:</label> */}
+                                    <input
+                                        type="password"
+                                        id="password"
+                                        placeholder='Password'
+                                        ref={userRef}
+                                        onChange={(e) => setPwd(e.target.value)}
+                                        value={pwd}
+                                        required
+                                    />
+                                    <button className='btn btn-login'>Sign In</button>
+                                </form>
+                                <div className="sign_flex">
+                                    <p>
+                                        Need an account?
+                                    </p>
+
+                                    <span className="line">
+                                        {/* put router link */}
+                                        <a href="#">Sign Up</a>
+                                    </span>
+
+                                </div>
+                            </section>
+                        ) :
+                        (
+                            <section>
+                                <h1>you're logged in</h1>
+                                <br />
+                                <p>
+                                    <a href="#">Go to home</a>
+                                </p>
+                            </section>
+                        )
+                }
+            </section>
         </>
     )
 }
